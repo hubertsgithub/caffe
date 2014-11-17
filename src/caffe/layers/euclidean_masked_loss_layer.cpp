b89112b1831ec03f3befc67f753d7c6e8e04c892
Blake Kovacs <kockafely@gmail.com>
2014-11-16 21:44:34 -0500

    Added norm_with_count parameter for euclidean loss

--- src/caffe/layers/euclidean_masked_loss_layer.cpp
+++ src/caffe/layers/euclidean_masked_loss_layer.cpp
@@ -28,6 +28,9 @@ template <typename Dtype>
 void EuclideanMaskedLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
   int count = bottom[0]->count();
+  bool norm_with_count = this->layer_param_.euclidean_param().norm_with_count();
+  float normwith = norm_with_count ? count : bottom[0]->num();
+
   caffe_sub(
       count,
       bottom[0]->cpu_data(),
@@ -41,7 +44,7 @@ void EuclideanMaskedLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bo
   		  diff_.mutable_cpu_data());
 
   Dtype dot = caffe_cpu_dot(count, diff_.cpu_data(), diff_.cpu_data());
-  Dtype loss = dot / bottom[0]->num() / Dtype(2);
+  Dtype loss = dot / normwith / Dtype(2);
   top[0]->mutable_cpu_data()[0] = loss;
 }
 
@@ -49,11 +52,15 @@ template <typename Dtype>
 void EuclideanMaskedLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
   int count = bottom[0]->count();
+  bool norm_with_count = this->layer_param_.euclidean_param().norm_with_count();
+
   // Do not compute back propagation for the mask layer!
   for (int i = 0; i < 2; ++i) {
     if (propagate_down[i]) {
+	  float normwith = norm_with_count ? bottom[i]->count() : bottom[i]->num();
       const Dtype sign = (i == 0) ? 1 : -1;
-      const Dtype alpha = sign * top[0]->cpu_diff()[0] / bottom[i]->num();
+      const Dtype alpha = sign * top[0]->cpu_diff()[0] / normwith;
+
       caffe_cpu_axpby(
           count,              // count
           alpha,                              // alpha
--- src/caffe/layers/euclidean_masked_loss_layer.cu
+++ src/caffe/layers/euclidean_masked_loss_layer.cu
@@ -11,6 +11,9 @@ template <typename Dtype>
 void EuclideanMaskedLossLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
   int count = bottom[0]->count();
+  bool norm_with_count = this->layer_param_.euclidean_param().norm_with_count();
+  float normwith = norm_with_count ? count : bottom[0]->num();
+
   caffe_gpu_sub(
       count,
       bottom[0]->gpu_data(),
@@ -24,19 +27,22 @@ void EuclideanMaskedLossLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bo
   		  diff_.mutable_gpu_data());
   Dtype dot;
   caffe_gpu_dot(count, diff_.gpu_data(), diff_.gpu_data(), &dot);
-  Dtype loss = dot / bottom[0]->num() / Dtype(2);
+  Dtype loss = dot / normwith / Dtype(2);
   top[0]->mutable_cpu_data()[0] = loss;
 }
 
 template <typename Dtype>
 void EuclideanMaskedLossLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  bool norm_with_count = this->layer_param_.euclidean_param().norm_with_count();
   int count = bottom[0]->count();
+
   // Do not compute back propagation for the mask layer!
   for (int i = 0; i < 2; ++i) {
     if (propagate_down[i]) {
+	  float normwith = norm_with_count ? bottom[i]->count() : bottom[i]->num();
       const Dtype sign = (i == 0) ? 1 : -1;
-      const Dtype alpha = sign * top[0]->cpu_diff()[0] / bottom[i]->num();
+      const Dtype alpha = sign * top[0]->cpu_diff()[0] / normwith;
       caffe_gpu_axpby(
           count,              // count
           alpha,                              // alpha
