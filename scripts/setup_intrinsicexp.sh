679cdb7b9d846ef6770b8343ce4eddd940301533
Blake Kovacs <kockafely@gmail.com>
2014-12-26 12:39:56 -0500

    Improved setup script

--- scripts/setup_intrinsicexp.sh
+++ scripts/setup_intrinsicexp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-pip install pypng pyamg redis msgpack-numpy
+sudo apt-get -y remove libopenblas-base
+sudo pip install pypng pyamg redis msgpack-numpy
 make pycaffe
+cd python/bkovacs
+celery -A lib.intrinsic.tasks worker --loglevel=info
 
