c1301c8026e275ebe2e675e5a69b35b378a95a6c
Blake Kovacs <kockafely@gmail.com>
2014-12-26 17:44:21 -0500

    Added missing line...

--- python/bkovacs/lib/intrinsic/tasks.py
+++ python/bkovacs/lib/intrinsic/tasks.py
@@ -2,6 +2,7 @@ from celery import Celery
 from lib.intrinsic.resulthandler import computeScoreJob_sendresults
 
 #app = Celery('comparison', backend='redis://localhost', broker='amqp://')
+app = Celery()
 app.config_from_object('lib.intrinsic.celeryconfig')
 
 @app.task
