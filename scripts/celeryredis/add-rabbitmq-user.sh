ca39e783f64bf23af43b110f4c3664cf7189079c
Blake Kovacs <kockafely@gmail.com>
2015-01-18 22:13:20 -0500

    Updated script, installing everything...

--- scripts/celeryredis/add-rabbitmq-user.sh
+++ scripts/celeryredis/add-rabbitmq-user.sh
@@ -2,9 +2,13 @@
 
 PASSWORD=$1
 
+cd python/bkovacs
+
 sudo rabbitmqctl status | grep "{rabbit,\"RabbitMQ\""
 celery --version
 
 sudo rabbitmqctl add_user rabbitmqroot $PASSWORD
 sudo rabbitmqctl set_permissions rabbitmqroot ".*" ".*" ".*"
 sudo rabbitmqctl list_users
+
+celery flower -A lib.intrinsic.tasks --address=0.0.0.0 --port=5555
--- scripts/celeryredis/install-celery-server.sh
+++ scripts/celeryredis/install-celery-server.sh
@@ -10,12 +10,18 @@ fi
 echo "Adding line to /etc/apt/sources.list"
 sudo bash -c 'echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list'
 wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+
 sudo apt-key add rabbitmq-signing-key-public.asc
 sudo apt-get -y update
 sudo apt-get -y install python-dev python-pip rabbitmq-server git htop
+sudo apt-get -y install gfortran libopenblas-dev liblapack-dev
+sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev
+sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler
+
 sudo pip install 'pip<1.6,>=1.5'
 sudo pip install numpy
-sudo pip install pypng pyamg redis msgpack-numpy Celery flower
+sudo pip install scipy
+sudo pip install pypng pyamg redis msgpack-numpy Celery flower Pillow
 
 mkdir /home/ubuntu/gitrepos
 cd /home/ubuntu/gitrepos
